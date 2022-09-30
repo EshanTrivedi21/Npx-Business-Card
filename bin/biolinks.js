758fa570debfc0c3e43e6473d55c11975b8b996f
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-09-30 16:22:21 +0530

    bugs fixed

--- bin/biolinks.js
+++ bin/biolinks.js
@@ -3,53 +3,48 @@
 import chalk from 'chalk';
 import boxen from 'boxen';
 
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as path from 'path';
 
-const options = {
-    padding: 1,
-    margin: 1,
-    borderStyle: 'round',
-    textAlignment: 'center',
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const styledDataFilename = '../info.json' 
 
 try {
-    const result = readFileSync(resolve(__dirname, "../info.json"));
+    const result = fs.readFileSync(path.resolve(__dirname, `${styledDataFilename}`));
     const userData = JSON.parse(result);
 
     const {
         first_name,
         last_name,
         current_status,
-        github_link,
-        linkedin_link,
     } = userData;
 
     const styledData = {
         firstName: chalk.blue.bold(first_name),
         lastName: chalk.blue.bold(last_name),
         currentStatus: chalk.blue.bold(current_status),
-        labelGithub: chalk.blue.bold("Github: "),
-        gitHub: chalk.blue(github_link),
-        labelLinkedin: chalk.blue.bold("Linkedin: "),
-        linkedIn: chalk.blue(linkedin_link),
     };
 
     const newline = "\n";
 
-    const output = 
-        newline + 
+    const output =  
         `${styledData.firstName} ${styledData.lastName}` +
-        newline +
-        newline +
-        `${styledData.currentStatus}` +
-        newline +
-        `${styledData.labelGithub} ${styledData.gitHub}` +
-        newline +
-        `${styledData.labelLinkedin} ${styledData.linkedIn}`;
+        newline + newline +
+        `${styledData.currentStatus}`;
 
-        console.log(chalk.bgGreen.bold(boxen(output, options)));
+    const options = {
+        padding: 1,
+        margin: 1,
+        textAlignment: 'center',
+    };
+
+    console.log(chalk.bgGreen.bold(boxen(output, options)));
 
 }   catch (err) {
-    console.log(chalk.bgRed.bold('Cannot read data.json file!'));
+    console.log(chalk.bgRed.bold(`Cannot read ${styledDataFilename} file!`));
+    console.log(chalk.bgRed.bold(err.message));
 }
